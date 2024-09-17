26a766a1f52ecd4607bf5fbe7df7e7b4829f4459
Tamaghno Ghosh <tamaghnog@gmail.com>
2024-09-17 19:31:08 +0530

    Modified

--- String/check_pangram.cpp
+++ String/check_pangram.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <cctype> // for isalpha and tolower
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cctype> // for isalpha and tolower
 
 using namespace std;
 
@@ -9,13 +9,13 @@ bool isPangram(const string& str) {
     vector<bool> alphabet(26, false);
 
     for(char ch : str) {
-        if(isalpha(ch)) {
+        if(isalpha(ch)) {       ////isalpha(ch) checks if the character ch is an alphabetic letter (either uppercase or lowercase)
             alphabet[tolower(ch) - 'a'] = true;
         }
     }
 
     for(int i = 0; i < 26; ++i) {
-        if(!alphabet[i]) {
+        if(!alphabet[i]) {          
             return false;
         }
     }
@@ -38,3 +38,7 @@ int main() {
 }
 
 // A pangram is a sentence that contains every letter of the alphabet at least once.
+//alphabet[tolower(ch) - 'a'] = true;:
+
+// This marks the letter as present by setting the corresponding index in the alphabet array to true.
+// The alphabet array has 26 elements, each corresponding to a letter from 'a' to 'z'. Initially, all are set to false, and this line changes it to true when the letter is found in the string.
