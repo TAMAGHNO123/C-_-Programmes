0ebeef304942c68d53507bd6bdcf326cf108e381
Tamaghno Ghosh <tamaghnog@gmail.com>
2024-11-17 19:16:09 +0530

    Implement maxScore function to calculate maximum subarray sum and add a main function for testing

--- Array Interview Questions/Max_Subarr_Score.cpp
+++ Array Interview Questions/Max_Subarr_Score.cpp
@@ -9,5 +9,28 @@ int maxScore(vector<int>& arr){
     int n = arr.size();
     int maxScore = INT_MIN;
 
-    
+    for(int i = 0; i < n; ++i){
+        int smallest = INT_MAX, secondSmallest = INT_MAX;
+
+            for(int j = i; j < n; ++j){
+                if(arr[j] < smallest){
+                    secondSmallest = smallest;
+                    smallest = arr[j];
+                }
+                else if(arr[j] < secondSmallest){
+                    secondSmallest = arr[j];
+                }
+                //calculating value if we get the least elements
+                if(secondSmallest != INT_MAX){
+                    maxScore = max(maxScore, smallest + secondSmallest);
+                }
+            }
+    }
+return maxScore;
+}
+
+int main(){
+    vector<int> arr = {3,1,2,6};
+    cout << "Maximum subarray sum of given vector of inetegers will be : "<< maxScore(arr) << " ";
+    return 0;
 }
